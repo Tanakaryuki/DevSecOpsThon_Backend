aa899e927c7dc6727da3453158dcca695a9509ba
Tanakaryuki <tnk3257.rt@gmail.com>
2024-04-28 22:25:02 +0900

    fix: streamingが動くように

--- api/routers/lmm.py
+++ api/routers/lmm.py
@@ -7,16 +7,19 @@ from llm.chat import qa_system
 router = APIRouter()
 
 @router.get("/streaming",tags=["lmm"])
-async def get_streaming() -> StreamingResponse:
+async def get_streaming(question: str) -> StreamingResponse:
+    message, sources = qa_system(question)
     async def generate():
-        for i in range(10):
-            yield f"data: {i}\n\n"
-            await asyncio.sleep(0.5)
+        for char in message:
+            await asyncio.sleep(0.05)
+            yield f"data: {char}\n\n"
+        for char in sources:
+            await asyncio.sleep(0.01)
+            yield f"data: {char}\n\n"
 
     return StreamingResponse(generate(), media_type="text/event-stream")
 
 @router.get("/question",tags=["lmm"])
-async def get_question() -> dict:
-    text = "ここに質問が来る"
-    message, sources = qa_system(text)
+async def get_question(question: str) -> dict:
+    message, sources = qa_system(question)
     return {"message": message, "sources": sources}
